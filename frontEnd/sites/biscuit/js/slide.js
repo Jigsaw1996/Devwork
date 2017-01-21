79f63501896d7bfd5cdd4b14788e3b8a1d0276b9
Sam <smoore9617@gmail.com>
2017-01-21 01:07:18 +0000

    Biscuit

--- frontEnd/sites/biscuit/js/slide.js
+++ frontEnd/sites/biscuit/js/slide.js
@@ -1,24 +1,27 @@
+addEventListener("load", function() {
+	
+	// Buttons
+	var homeBtn = document.getElementById("homeBtn");
+	var contactBtn = document.getElementById("contactBtn");
 
-// Buttons
-var homeBtn = document.getElementById("homeBtn");
-var contactBtn = document.getElementById("contactBtn");
+	// Individual Slides 
+	var slides = document.getElementsByClassName("slide");
+	var firstSlide = slides[0];
+	var secondSlide = slides[1];
 
-// Individual Slides 
-var slides = document.getElementsByClassName("slide");
-var firstSlide = slides[0];
-var secondSlide = slides[1];
+	// Sliding Motion
+	function slideHome() {
+		firstSlide.style.left = 0;
+		secondSlide.style.left = 100 + "%";
+	}
 
-// Sliding Motion
-function slideHome() {
-	firstSlide.style.left = 0;
-	secondSlide.style.left = 100 + "%";
-}
+	function slideContact() {
+		firstSlide.style.left = "-" + 100 + "%";
+		secondSlide.style.left = 0;
+	}
 
-function slideContact() {
-	firstSlide.style.left = "-" + 100 + "%";
-	secondSlide.style.left = 0;
-}
+	// Event Listeners
+	homeBtn.addEventListener("click", slideHome);
+	contactBtn.addEventListener("click", slideContact);
 
-// Event Listeners
-homeBtn.addEventListener("click", slideHome);
-contactBtn.addEventListener("click", slideContact);
+});
---
+++ frontEnd/sites/biscuit/readme.md
@@ -0,0 +1,11 @@
+# Site Specification
+
+* HTML5
+* CSS3
+* JS
+
+# Gives examples of:
+
+* Mobile First Design
+* JS DOM manipulation
+* One Page Design
