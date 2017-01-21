7b2ad2e58772845e6106a5db61599e28e39e9050
Sam <smoore9617@gmail.com>
2017-01-21 00:45:29 +0000

    Biscuit Changes

--- frontEnd/sites/biscuit/js/slide.js
+++ frontEnd/sites/biscuit/js/slide.js
@@ -4,7 +4,6 @@ var homeBtn = document.getElementById("homeBtn");
 var contactBtn = document.getElementById("contactBtn");
 
 // Individual Slides 
-var container = document.getElementById("resize");
 var slides = document.getElementsByClassName("slide");
 var firstSlide = slides[0];
 var secondSlide = slides[1];
@@ -13,13 +12,11 @@ var secondSlide = slides[1];
 function slideHome() {
 	firstSlide.style.left = 0;
 	secondSlide.style.left = 100 + "%";
-	container.style.height = 340 + "px";
 }
 
 function slideContact() {
 	firstSlide.style.left = "-" + 100 + "%";
 	secondSlide.style.left = 0;
-	container.style.height = 400 + "px";
 }
 
 // Event Listeners
