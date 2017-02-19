b3969fbf15e9a2e1b7b2ce7f37754f7e23f09d59
Sam <smoore9617@gmail.com>
2017-02-19 23:28:26 +0000

    JS Functions

---
+++ frontEnd/javascriptExamples/ifStatements/js/reColour.js
@@ -0,0 +1,10 @@
+var green = document.getElementById("onePlusOne");
+var red = document.getElementById("twoPlusTwo");
+
+if ((1 + 1) == 2) {
+	green.style.color = "green";
+}
+
+if ((2 + 2) == 5) {
+	red.style.color = "red";
+}
---
+++ frontEnd/javascriptExamples/loops/js/loop.js
@@ -0,0 +1,3 @@
+for (x = 1; x <=5; x++) {
+	console.log(x);
+}
