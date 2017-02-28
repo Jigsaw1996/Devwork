058a5829a687a0bd74f3428b4a8cabcb94ebae3a
Sam <smoore9617@gmail.com>
2017-02-28 00:06:35 +0000

    JQuery

--- frontEnd/sites/biscuit/js/slide.js
+++ frontEnd/sites/biscuit/js/slide.js
@@ -19,9 +19,34 @@ addEventListener("load", function() {
 		firstSlide.style.left = "-" + 100 + "%";
 		secondSlide.style.left = 0;
 	}
+	
+	// Info Squares
+	var fourK = document.getElementById("fourK");
+	var editing = document.getElementById("editing");
+	var upload = document.getElementById("upload");
+	
+	// Popups
+	var popupFourK = document.getElementById("popupFourK");
+	var popupEditing = document.getElementById("popupEditing");
+	var popupUpload = document.getElementById("popupUpload");
+	
+	// Popup Function
+	function openTab(tab) {
+		tab.style.display = "block";
+		document.documentElement.style.overflow = 'hidden';
+	}	
 
 	// Event Listeners
 	homeBtn.addEventListener("click", slideHome);
 	contactBtn.addEventListener("click", slideContact);
-
+	
+	fourK.addEventListener("click", function() {
+		openTab(popupFourK);
+	});
+	editing.addEventListener("click", function() {
+		openTab(popupEditing);
+	});
+	upload.addEventListener("click", function() {
+		openTab(popupUpload);
+	});
 });
