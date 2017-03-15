4041197e1e29aaab30f66698c01fa01223d118f8
Sam <smoore9617@gmail.com>
2017-03-15 23:22:39 +0000

    JS and JQuery, API etc

---
+++ frontEnd/jqueryExamples/api/js/api.js
@@ -0,0 +1,13 @@
+ $.ajax({
+ 
+url:"https://api.themoviedb.org/3/movie/popular?page=1&api_key=35e16679c616a21b9ddebb66272c5902",
+        dataType: "json",
+        complete: function (response) {
+            // response contains number of items one of which
+            // is any associated data .responseText
+            $('#output').html(response.responseText);
+        },
+        error: function () {
+            $('#output').html('Bummer: there was an error!');
+        },
+    });
