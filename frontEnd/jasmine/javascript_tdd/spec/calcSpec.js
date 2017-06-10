ff7f751198f548944b0d1d425ac0c5b04f3e4d30
Sam <smoore9617@gmail.com>
2017-06-10 17:05:04 +0100

    Calc Updates

--- frontEnd/jasmine/javascript_tdd/spec/calcSpec.js
+++ frontEnd/jasmine/javascript_tdd/spec/calcSpec.js
@@ -1,15 +1,68 @@
 describe("Calculator", function() {
-    describe('Addition function', function() {
-        it('Add two numbers together and return the result', function() {
-            expect(addition(2,2)).toBe(4);
-        });
-		it("Second run with different numbers", function() {
-			expect(addition(7, 19)).toBe(26);
-		});
-		it("should have called the alert function if either number is undefined", function() {
-			spyOn(window, "alert");
-			addition("Hello");
-			expect(window.alert).toHaveBeenCalledWith("Arguments must both be a number");
-		});
+ 
+    beforeEach(function() {
+        calc = new Calculator();
     });
+    describe('Addition function', function() {
+        it('should add two numbers together and return the result', function() {
+            calc.add(2,2);
+            expect(calc.value).toBe(4);
+        });
+ 
+        it("should not return 4 if the numbers given don't add up to 4", function() {
+            calc.add(12, 7);
+            expect(calc.value).toBe(19);
+        });
+ 
+        it("should have called the alert function if the number given is undefined", function() {
+            spyOn(window, "alert");
+            calc.add();
+            expect(window.alert).toHaveBeenCalledWith("Addition argument must be a number");
+        });
+    });
+    describe("Subtraction function", function() {
+		it("Should subtract two numbers and return the result", function() {
+			calc.subtract(4,2);
+			expect(calc.value).toBe(2);
+		});
+		it("Should return another test with different numbers and correct result", function() {
+			calc.subtract(10,4);
+			expect(calc.value).toBe(6);
+		});
+        it("should have called the alert function if the number given is undefined", function() {
+            spyOn(window, "alert");
+            calc.subtract();
+            expect(window.alert).toHaveBeenCalledWith("Subtract argument must be a number");
+        });
+	});
+    describe("Multiply function", function() {
+		it("Should multiply two numbers and return the result", function() {
+			calc.multiply(5,10);
+			expect(calc.value).toBe(50);
+		});
+		it("Should return another test with different numbers and correct result", function() {
+			calc.multiply(2,4);
+			expect(calc.value).toBe(8);
+		});
+        it("should have called the alert function if the number given is undefined", function() {
+            spyOn(window, "alert");
+            calc.multiply();
+            expect(window.alert).toHaveBeenCalledWith("Multiplication argument must be a number");
+        });
+	});
+    describe("Division function", function() {
+		it("Should divide two numbers and return the result", function() {
+			calc.divide(10,2);
+			expect(calc.value).toBe(5);
+		});
+		it("Should return another test with different numbers and correct result", function() {
+			calc.divide(24,4);
+			expect(calc.value).toBe(6);
+		});
+        it("should have called the alert function if the number given is undefined", function() {
+            spyOn(window, "alert");
+            calc.divide();
+            expect(window.alert).toHaveBeenCalledWith("Division argument must be a number");
+        });
+	});
 });
--- frontEnd/jasmine/javascript_tdd/src/calc.js
+++ frontEnd/jasmine/javascript_tdd/src/calc.js
@@ -1,8 +1,39 @@
-function addition(num1, num2) {
-	if (typeof(num1) == "number" && typeof(num2) == "number") {
-		return num1 + num2;
-	}
-	else {
-		alert("Arguments must both be a number");
-	}	
+Calculator = function() {
+    this.value = 0;
+};
+
+Calculator.prototype.error = function(reason) {
+	alert(reason + " argument must be a number");
+}
+ 
+Calculator.prototype.add = function(number1, number2) {
+    if ((typeof(number1) == "number") && (typeof(number2) == "number")) {
+        this.value = number1 + number2;
+    } else {
+        this.error("Addition");
+    }
+};
+
+Calculator.prototype.subtract = function(number1, number2) {
+    if ((typeof(number1) == "number") && (typeof(number2) == "number")) {
+        this.value = number1 - number2;
+    } else {
+        this.error("Subtract");
+    }
+}
+
+Calculator.prototype.multiply = function(number1, number2) {
+    if ((typeof(number1) == "number") && (typeof(number2) == "number")) {
+        this.value = number1 * number2;
+    } else {
+        this.error("Multiplication");
+    }
+}
+
+Calculator.prototype.divide = function(number1, number2) {
+    if ((typeof(number1) == "number") && (typeof(number2) == "number")) {
+        this.value = number1 / number2;
+    } else {
+        this.error("Division");
+    }
 }
